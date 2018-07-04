47eb4e5b5236023b544318bded77ef3fa4c88af3
Tejas Jasani <bharatappguru@gmail.com>
2018-07-04 14:09:41 +0530

    Initial Commit : Blog :sprite-sheet-animation-in-unity Author ; Vivek Nakum

---
+++ Assets/WithAnimation/ScriptToAnimate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptToAnimate : MonoBehaviour
+{
+    public Animator animator;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            animator.SetTrigger("Idle");
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            animator.SetTrigger("Kick");
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetTrigger("Walk");
+        }
+    }
+   
+}
